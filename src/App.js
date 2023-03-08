432e83820a1307e8e9312e0479662f23f8c2d9da
Maria do Socorro de Lima <maria.slima@saude.gov>
2023-03-08 12:28:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite o <code>src/App.js</code> e salve e recarregue.
+       
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Aprendendo React
         </a>
       </header>
     </div>
